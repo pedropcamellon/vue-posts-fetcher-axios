6603dfc4b94d7960cf00efbecbab6316a878a325
pedropcamellon <pedropcamellon@gmail.com>
2023-03-29 14:52:33 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios"
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+      .then(response => {
+        // handle success
+        console.log(response.data)
+        this.posts = [...response.data]
+      });
+  }
+}
+</script>
+
+<template>
+  <div v-for="p in posts">
+    <h2>{{ p.title }}</h2>
+    <p>{{ p.body }}</p>
+  </div>
+</template>
+
+<style scoped></style>
